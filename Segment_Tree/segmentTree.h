e1ca16747799fd7d9dfd1d42c3febdde61f41783
Golden-Gread <778966179@qq.com>
2026-07-02 10:56:19 +0800

    modified:   .vscode/tasks.json 	modified:   Segment_Tree/segmentTree.h

--- Segment_Tree/segmentTree.h
+++ Segment_Tree/segmentTree.h
@@ -1,6 +1,18 @@
+#include <vector>
+
 using namespace std;
 
 class segmentTree{
 private:
-	vector<int> 
+	vector<int> original_data;
+    vector<int> tree;
+    vector<int> lazy;
+public:
+    void buildTree(int l,int r,int p);
+    void range_add(int l,int r,int v,int s,int t,int p);
+    int getSum(int l,int r,int s,int t,int p);
 };
+
+inline void segmentTree::buildTree(int l,int r,int p){
+
+}
