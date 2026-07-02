f8394b6e063dc66dfb4180f87a7b17d44475a4cf
Golden-Gread <778966179@qq.com>
2026-07-02 11:21:03 +0800

    new file:   Segment_Tree/P3372-2.cpp modified:   Segment_Tree/segmentTree.h

--- Segment_Tree/segmentTree.h
+++ Segment_Tree/segmentTree.h
@@ -9,10 +9,22 @@ private:
     vector<int> lazy;
 public:
     void buildTree(int l,int r,int p);
+    void pushDown();
     void range_add(int l,int r,int v,int s,int t,int p);
     int getSum(int l,int r,int s,int t,int p);
 };
 
 inline void segmentTree::buildTree(int l,int r,int p){
+    if(l==r){
+        tree[p]=original_data[l];
+        return ;
+    }
+    int mid=l+((r-l)>>1);
+    buildTree(l,mid,p*2);
+    buildTree(mid+1,r,p*2+1);
+    tree[p]=tree[p*2]+tree[p*2+1];
+}
+
+inline void segmentTree::range_add(int l,int r,int v,int s,int t,int p){
 
 }
