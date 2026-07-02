74c400d9d392c895222148f71ae923b854314419
Golden-Gread <778966179@qq.com>
2026-07-02 16:17:03 +0800

    modified:   Segment_Tree/P3372-2.cpp 	new file:   Segment_Tree/P3372-2.exe 	new file:   Segment_Tree/P4556.cpp

--- Segment_Tree/P3372-2.cpp
+++ Segment_Tree/P3372-2.cpp
@@ -27,7 +27,7 @@ void pushDown(int p,int l,int r){
 	int mid=(l+r)>>1;
 	
 	if(tree[p].lc==-1) tree[p].lc=newNode();
-	if(tree[p].rc==-1) tree[p].rc=newNode();
+	if(tree[p].rc==-1)tree[p].rc=newNode();
 	
 	ll lazy_val=tree[p].lazy;
 	int left=tree[p].lc,right=tree[p].rc;
@@ -37,11 +37,57 @@ void pushDown(int p,int l,int r){
 	tree[p].lazy=0;
 }
 
-void range_adding(int l,int r,int v,int s,int t,int& p){
-	
+void range_adding(int l,int r,ll v,int s,int t,int& p){
+	if(p==-1){
+		p=newNode();
+	}
+	if(l<=s and t<=r){
+		tree[p].val+=(t-s+1)*v;
+		tree[p].lazy+=v;
+		return ;
+	}
+	ll mid=s+((t-s)>>1);
+	pushDown(p,s,t);
+	if(l<=mid) range_adding(l,r,v,s,mid,tree[p].lc);
+	if(r>mid) range_adding(l,r,v,mid+1,t,tree[p].rc);
+	//tree[p].val=tree[tree[p].lc].val+tree[tree[p].rc].val;
+	tree[p].val = 0;
+	if (tree[p].lc != -1) tree[p].val += tree[tree[p].lc].val;
+	if (tree[p].rc != -1) tree[p].val += tree[tree[p].rc].val;
 }
 
 
+ll getSum(int l,int r,int s,int t,int& p){
+	if(r<s or l>t or p==-1) return 0;
+	if(l<=s and t<=r) return tree[p].val;
+	ll mid=s+((t-s)>>1);
+	pushDown(p,s,t);
+	ll sum=0;
+	if(l<=mid) sum+=getSum(l,r,s,mid,tree[p].lc);
+	if(r>mid) sum+=getSum(l,r,mid+1,t,tree[p].rc);
+	return sum;
+}
+
 int main(){
+	int n,m;cin>>n>>m;
+	
+	int root=newNode();
+	for(int i=1;i<=n;i++){
+		int x;cin>>x;
+		range_adding(i,i,x,1,n,root);
+	}
+	for(int i=0;i<m;i++){
+		int code;cin>>code;
+		if(code==1){
+			ll x,y,k;cin>>x>>y>>k;
+			range_adding(x,y,k,1,n,root);
+		}
+		else{
+			int x,y;cin>>x>>y;
+			cout<<getSum(x,y,1,n,root)<<endl;
+		}
+	}
+	
+	return 0;
 	
 }
