97011b4976b28e8d2bae60ecce1097605a758693
Golden-Gread <778966179@qq.com>
2026-07-03 19:03:07 +0800

    new file:   Segment_Tree/P5227.cpp

---
+++ Segment_Tree/P5227.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef pair<int,int> pii;//(dist,to)
+
+int dfs(vector<vector<pii>>& g,int node,int ans,vector<int>& visited){
+	if(visited[node]==1){
+        return ans;
+    }
+    visited[node]=1;
+    for(auto& [dist,to]:g[node]){
+        ans+=dfs(g,to,ans,visited);
+    }
+}
+
+int connectGraph(vector<vector<pii>>& g){
+	vector<int> visited(g.size(),0);
+    int ans=0;
+    dfs(g,0,ans,visited);
+    return ans;
+}
+
+
+
+
+int main(){
+	
+}
