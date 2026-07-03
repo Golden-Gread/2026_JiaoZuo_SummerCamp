b1551b8770a58e248fa380bac4b81bf010409169
Golden-Gread <778966179@qq.com>
2026-07-03 14:03:04 +0800

    modified:   Segment_Tree/P5787.cpp

--- Segment_Tree/P5787.cpp
+++ Segment_Tree/P5787.cpp
@@ -1,23 +1,23 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
+const ll M=2e5+4;
 
-const int M=2e5+4;
-
-int n=0;
+ll n=0;
 
 struct Edge{
-	int u,v;
+	ll u,v;
 	Edge():u(-1),v(-1){}
 } e[M];
 
 //rollback DSU
 
-vector<int> fa(M*2,0),size(M*2,0);
-struct hist{int u,v,add_sz;};
+vector<ll> fa(M*2,0),size(M*2,0);
+struct hist{ll u,v,add_sz;};
 stack<hist> history={};
 
-int find(int x){
+ll find(ll x){
 	while(fa[x]!=x){
 		
 		x=fa[x];
@@ -25,8 +25,8 @@ int find(int x){
 	return x;
 }
 
-bool unite(int u,int v){
-	int fa_u=find(u),fa_v=find(v);
+bool unite(ll u,ll v){
+	ll fa_u=find(u),fa_v=find(v);
 	if(fa_u==fa_v){ history.push({-1, -1, -1});return false;}
 	
 	if(size[fa_u]>size[fa_v])
@@ -42,13 +42,13 @@ bool unite(int u,int v){
 void rollback(){
 	if(history.empty()) return;
 	auto it=history.top();history.pop();
-	int u=it.u,v=it.v,ch_val=it.add_sz;
+	ll u=it.u,v=it.v,ch_val=it.add_sz;
 	if(u==-1) return;
 	fa[u]=u;
 	size[v]-=ch_val;
 }
 
-void rollback_to_point(int x){
+void rollback_to_poll(ll x){
 	while(history.size()>x){
 		rollback();
 	}
@@ -56,73 +56,62 @@ void rollback_to_point(int x){
 
 
 // Time Segment Tree
-vector<int> tree[M*4];
+vector<ll> tree[M*4];
 
-void adding_edge(int node,int nd_l,int nd_r,int ed_l,int ed_r,int edge){
+void adding_edge(ll node,ll nd_l,ll nd_r,ll ed_l,ll ed_r,ll edge){
 	if(ed_l<=nd_l and nd_r<=ed_r){  //completely covered
 		tree[node].push_back(edge);
 		return ;
 	}
-	int mid=(nd_l+nd_r)>>1;
+	ll mid=(nd_l+nd_r)>>1;
 	if(ed_l<=mid) adding_edge(node*2,nd_l,mid,ed_l,ed_r,edge);
 	if(ed_r>mid) adding_edge(node*2+1,mid+1,nd_r,ed_l,ed_r,edge);
 }
 
-void dfs(int node,int l,int r,bool& is_right){
-	if(l>r) return ;
+void dfs(ll node,ll l,ll r,bool is_right){
+
 	
-	//if(!is_right) {
-	//	if(l == r) {
-	//		cout << "No\n";
-	//	} else {
-	//		int mid = (l + r) >> 1;
-	//		dfs(node * 2, l, mid, is_right);
-	//		dfs(node * 2 + 1, mid + 1, r, is_right);
-	//	}
-	//	return;
-	//}
+
+	ll timepoll=history.size();
 	
-	int timepoint=history.size();
-	
-	for(int edge:tree[node]){
-		int u=e[edge].u,v=e[edge].v;
-		
-		bool res1=unite(u,v);
-		//bool res2=unite(v,u+n);
-		if(!res1){
+	for(ll edge:tree[node]){
+		ll u=e[edge].u,v=e[edge].v;
+		bool res1=unite(u,v+n);
+		bool res2=unite(v,u+n);
+		if(!res1 or !res2){
 			is_right=0;
 			break;
 		}
 	}
 	
 	if(l==r){
-		printf("%s",((is_right)?"Yes":"No"));
+		prllf("%s",((is_right)?"Yes":"No"));
 	}
 	else{
-		int mid=(l+r)>>1;
+		ll mid=(l+r)>>1;
 		dfs(node*2,l,mid,is_right);
 		dfs(node*2+1,mid+1,r,is_right);
 	}
 	
-	rollback_to_point(timepoint);
+	rollback_to_poll(timepoll);
 }
 
 int main(){
-	int m,k;scanf("%d %d %d",&n,&m,&k);
+	ll m,k;scanf("%d %d %d",&n,&m,&k);
 	
-	for(int i=1;i<=2*n;i++){
+	for(ll i=1;i<=2*n;i++){
 		fa[i]=i;
 		size[i]=1;
 	}
 	
-	for(int i=1;i<=m;i++){
+	for(ll i=1;i<=m;i++){
 		int l,r;scanf("%d %d %d %d",&e[i].u,&e[i].v,&l,&r);
 		if(l<r){
 			adding_edge(1,1,k,l+1,r,i);
 		}
 	}
-	bool b=1;
-	dfs(1,1,k,b);
+	
+	dfs(1,1,k,1);
 	
 	return 0;
 }
