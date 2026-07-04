ef4c0248f513fdc60a1681c860f560dbfe19de38
Golden-Gread <778966179@qq.com>
2026-07-04 14:23:34 +0800

    modified:   string/P8306 Trie Tree.cpp

--- string/P8306 Trie Tree.cpp
+++ string/P8306 Trie Tree.cpp
@@ -2,8 +2,42 @@
 using namespace std;
 
 struct TrieNode {
-    char val;
-    vector<int> children;
-    TrieNode(char c) : val(c), children(26, -1) {}
+    unordered_map<char, TrieNode*> children;
+    bool isEnd;
+    TrieNode() : isEnd(false) {}
+    int dfs(){
+        for(auto child:children){
+            if(child.second!=nullptr and child.second->isEnd){
+                return child.second->dfs()+1;
+            }        
+        }
+    }
 };
+
+TrieNode* root = new TrieNode();
+
+void insert(string s){
+    TrieNode* cur = root;
+    for(char c:s){
+        if(cur->children.find(c)==cur->children.end()){
+            cur->children[c] = new TrieNode();
+        }
+        cur = cur->children[c];
+    }
+    cur->isEnd = true;
+}
+
+int find(TrieNode* cur,string s){
+    for(char c:s){
+        if(cur->children.find(c)==cur->children.end()) return -1;
+        cur = cur->children[c];
+    }
+
+    return cur->dfs() + 1;
+}
+
+int main(){
+    int t;cin>>t;
+    
+}
 
