7af4ce657f46af969b112d016cd4c13c7f11069b
Golden-Gread <778966179@qq.com>
2026-07-08 21:29:39 +0800

    new file:   .cph/.temp.cpp_fbb640a00e05a037fb3dfdb1a24be29c.prob 	modified:   .vscode/c_cpp_properties.json 	modified:   math/P4777.cpp 	new file:   math/P4777.exe 	modified:   temp.cpp 	modified:   temp.exe

--- math/P4777.cpp
+++ math/P4777.cpp
@@ -11,17 +11,52 @@ ll exGCD(ll a,ll b,ll &x,ll &y) {//Ax+By=GCD(a,b)
         return a;
     }
 
-    ll gcd = exGCD(b, a % b, x, y);
-    ll temp = x;
+    i128 gcd = exGCD(b, a % b, x, y);
+    i128 temp = x;
     x = y;
     y = temp - (a / b) * y;
     return gcd;
 }
 
 int main(){
-    int n;cin>>n;
+    freopen("D:/P4777_15.in","r",stdin);
+
+
+    int n;scanf("%d",&n);
     vector<ll> modN(n+1),modR(n+1);
     for(int i=1;i<=n;i++){
-        scanf("%lld %lld",modN[i],modR[i]);
+        scanf("%lld %lld",&modN[i],&modR[i]);
     }
+
+    ll M=modN[1];
+    ll ans=modR[1];
+    bool flag=true;
+    for(int i=2;i<=n;i++){
+        ll x,y;
+        i128 gcd=exGCD(M,modN[i],x,y);
+        i128 c=((modR[i]-ans)%modN[i]+modN[i])%modN[i];
+    
+        if(c%gcd!=0){
+            flag=false;
+            break;
+        }
+    
+        ll mod=modN[i]/gcd;
+        i128 temp=(i128)(x*c/gcd);
+        x=temp%mod;
+
+        ans=ans+x*M;
+        M=M/gcd*modN[i];
+        ans=(ans%M+M)%M;
+    }
+
+    if(flag){
+        printf("%lld\n",ans);
+    }else{
+        printf("-1\n");
+    }
+
+    return 0;
+
+
 }
