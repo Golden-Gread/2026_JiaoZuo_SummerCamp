7e32bd450d8773dbd93cc5e557005abf4eb27242
Golden-Gread <778966179@qq.com>
2026-07-08 14:17:54 +0800

    modified:   math/P3383.cpp

--- math/P3383.cpp
+++ math/P3383.cpp
@@ -1,32 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     
-    int n,q;cin>>n>>q;
+    int n, q;
+    scanf("%d %d\n",&n,&q);
     
-    int v[10000000+1]={0};
     vector<int> prime;
-    for(int i=2;i<=n;i++){
-        if(v[i]==0){
+    vector<int> v(n + 1, 0);
+    
+
+    prime.reserve(n / max(1, (int)log(n)));
+    
+    for (int i = 2; i <= n; i++) {
+        if (v[i] == 0) {
+            v[i] = i;
             prime.push_back(i);
-            v[i]=i;
-            //phi[i]=i-1;
         }
-        for(auto p:prime){
-            if(1LL*i*p>n or p>v[i]){
-                break;
-            }
-            v[i*p]=p;
+        for (int p : prime) {
+            long long t = 1LL * i * p;
+            if (t > n) break;
+            v[t] = p;
+            if (p == v[i]) break;
         }
     }
-
     
-    
-    for(int i=0;i<q;i++){
-        int x;scanf("%d",&x);
+    while (q--) {
+        int x;
+        scanf("%d",&x);
         printf("%d\n",prime[x-1]);
+        
     }
-
+    
     return 0;
 }
