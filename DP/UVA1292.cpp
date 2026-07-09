65a2b4fa03cbab7671292e0e9dad27a3258e04af
Golden-Gread <778966179@qq.com>
2026-07-09 19:41:19 +0800

    modified:   DP/UVA1292.cpp

--- DP/UVA1292.cpp
+++ DP/UVA1292.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    while(scanf("%d\n",&n)!=EOF){
+        vector<vector<int>> tree(n+1,vector<int>());
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        int u,childNum;
+        for(int i=0;i<n;i++){
+            
+            scanf()
+
+        }
+    }
 }
