3fa5d02bf7a9d8ebb7c9838f9d1e56e13b1f478a
Golden-Gread <778966179@qq.com>
2026-07-10 07:21:33 +0800

    modified:   DP/UVA1292.cpp

--- DP/UVA1292.cpp
+++ DP/UVA1292.cpp
@@ -1,17 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void dfs(int now,vector<vector<int>>& tree,vector<vector<int>>& dp){
+    dp[now][1]=1;
+    for(int child:tree[now]){
+        dfs(child,tree,dp);
+        dp[now][0]+=dp[child][1];
+        dp[now][1]+=min(dp[child][0],dp[child][1]);
+    }
+}
+
+
+
 int main(){
     int n;
+    vector<vector<int>> tree;
+    vector<int> deg;
+    vector<vector<int>> dp;
     while(scanf("%d\n",&n)!=EOF){
-        vector<vector<int>> tree(n+1,vector<int>());
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        tree.resize(n+1,vector<int>());
+        deg.resize(n+1,0);
+        dp.resize(n+1,vector<int>(2,0));
+
         int u,childNum;
         for(int i=0;i<n;i++){
-            
             scanf("%d:(%d)",&u,&childNum);
-            
-
+            for(int j=0;j<childNum;j++){
+                int child;
+                scanf(" %d",&child);
+                deg[child]++;
+                tree[u].push_back(child);
+            }
+            scanf("\n");
         }
+        int rt;
+        for(int i=1;i<=n;i++){
+            if(deg[i]==0){
+                rt=i;
+                dfs(i,tree,dp);
+                break;
+            }
+        }
+        printf("%d\n",min(dp[rt][0],dp[rt][1]));
+        tree.clear();
+        deg.clear();
+        dp.clear();
     }
 }
