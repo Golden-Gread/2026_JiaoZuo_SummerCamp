22567aae10be321be4aeaa203412fc77db81fa1e
Golden-Gread <778966179@qq.com>
2026-07-10 20:09:45 +0800

    modified:   DP/P2016.cpp

--- DP/P2016.cpp
+++ DP/P2016.cpp
@@ -4,8 +4,13 @@ const int INF=1e9+7;
 
 void DFS(int now,int fa,vector<vector<int>>& tree,vector<vector<int>>& dp){
     dp[now][1]=1;dp[now][0]=0;
-    
-    for(int i)
+
+    for(int to:tree[now]){
+        if(to==fa) continue;
+        DFS(to,now,tree,dp);
+        dp[now][0]+=dp[to][1];
+        dp[now][1]+=min(dp[to][0],dp[to][1]);
+    }
 }
 
 
@@ -23,6 +28,8 @@ int main(){
     }
 
     vector<vector<int>> dp(n+1,vector<int>(n+1,INF));
-    
+    DFS(0,-1,tree,dp);
+    cout<<min(dp[0][0],dp[0][1])<<endl;
+    return 0;    
     
 }
