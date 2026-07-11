7984aa53fa92d485762d4b7eb1035b399bfcbbc3
Golden-Gread <778966179@qq.com>
2026-07-12 07:05:27 +0800

    new file:   "7_9_\347\273\204\345\220\210\350\256\241\346\225\260_5.pdf" 	new file:   AtCoder ABC 466/.cph/.T5.cpp_9ad5c8d0763cee26b5f32b14152c6477.prob 	new file:   AtCoder ABC 466/T4.cpp 	new file:   AtCoder ABC 466/T5.cpp 	new file:   "\346\240\221.pdf"

---
+++ AtCoder ABC 466/T4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;cin>>n>>m;
+    set<int> xz,yz;
+    long long ans=0;
+    for(int i=0;i<m;i++){
+        int a,b;cin>>a>>b;
+        if(xz.find(a)!=xz.end()||yz.find(b)!=yz.end()){
+            
+        }
+    }
+}
